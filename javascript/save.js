31147e75cee9e179b6a79159d2ec5bf14a3b1e58
mudgrunt <fullertonkyler@gmail.com>
2017-10-11 08:11:35 -0500

    Delete save.js

--- javascript/save.js
+++
@@ -1,8 +0,0 @@
-//Saving image and displaying through html2canvas
-var takeScreenShot = function() {
-    html2canvas(document.getElementById("target"), {
-        onrendered: function (canvas) {
-            document.body.appendChild(canvas);
-        },
-    });
-}
