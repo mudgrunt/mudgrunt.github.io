2d60685e8a71af9c7960ab54b59b259a14461529
mudgrunt <fullertonkyler@gmail.com>
2017-10-11 08:07:08 -0500

    Add files via upload

--- javascript/save.js
+++ javascript/save.js
@@ -1,5 +1,5 @@
 //Saving image and displaying through html2canvas
-window.takeScreenShot = function() {
+var takeScreenShot = function() {
     html2canvas(document.getElementById("target"), {
         onrendered: function (canvas) {
             document.body.appendChild(canvas);
